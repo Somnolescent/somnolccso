8a20989c64eef0957bff88c7b27c2609038d590b
Cramblioni <trystanpvincent@gmail.com>
2024-02-08 11:32:17 +0000

    Wrote new query parser. Bodjed query solver code
    I wrote a new query parser, to solve issue #3, and bodged the query
    solving code in the process. The query parsing code has been *kinda*
    patched. The query parsing code can be found in the `parse_query`
    function.
    
    If there is an error with it, please either fix it or inform me.

--- ccso.py
+++ ccso.py
@@ -84,10 +84,80 @@ def find_all_fields():
             if not field in unique_fields:
                 unique_fields.append(field)
 
+
+# for parsing fields
+# returns either
+#   (true, (dict[str, str], list[str]))
+#       parse succeeded and the query is returned
+# or
+#   (false, str)
+#       parse failed and a message is returned.
+def parse_query(query):
+    if query.startswith("query "):
+        query = query[len("query "):]
+    # assuming queries are structured as
+    #       *criterion ["return" *fields]
+
+    # regex dump
+    #       criterion: r'(\S*)="([^"]*)"'
+    
+    index = 0
+    criteria = {}
+    # phase 1: Criteria
+    # compiling this because then match accepts a start index :)
+    pat_criterion = re.compile(r'(\S*)="([^"]*)"', re.ASCII) 
+    whitespace = re.compile(r'\s*', re.ASCII)
+    inphase = True
+    while inphase:
+        index = whitespace.match(query, index).end()
+        if index >= len(query):
+            break
+        pars = pat_criterion.match(query, index)
+        if pars is None:
+            break
+        index = pars.end()
+        if pars.group(1) not in unique_fields:
+            return False, f"507:Field {pars.group(1)} not recognised"
+        criteria[pars.group(1)] = pars.group(2)
+    
+    if len(criteria) == 0:
+        return False, "512:Illegal Value. No search criteria provided"
+
+    # phase 2: the return
+    index = whitespace.match(query, index).end()
+    if index >= len(query):
+        return True, (criteria, ["all"])
+
+    if not query[index:].startswith("return"):
+        return False, "512: Illegal value. please don't be wrong :("
+    index += len("return")
+    # phase 3: return fields
+    # being lazy :)
+    returns = query[index:].split()
+    if len(returns) == 0:
+        # swapped it over to match the current implementation
+        # logging.info("no return fields, coercing to `always_fields`")
+        # returns.extend(always_fields)
+        logging.info("empty return clause, coercing to \"all\"")
+        returns.append("all")
+    else:
+        # this avoids repeating the always_fields implicitly
+        for alf in always_fields:
+            if alf not in returns:
+                logging.info(f"field {alf:r} not included, implicitly adding")
+                returns.append(alf)
+        
+    for ret in returns:
+        if ret not in unique_fields:
+            return False, f"507:Return field {ret} not recognised"
+    return True, (criteria, returns)
+
+
 logger.info('SomnolCCSO v0.3 started')
 
 # Read database for first boot
 reload_db()
+find_all_fields()
 
 class PhProtocol(asyncio.Protocol):
     # Start the connection, print to console who dis
@@ -99,7 +169,7 @@ class PhProtocol(asyncio.Protocol):
         global last_reload
 
         request = data.decode('utf-8')
-
+        
         # Lots of command scrubbing
         # Mosaic likes to do percent encoding, seen other clients add extra line breaks to their commands
         request = unquote(request)
@@ -160,104 +230,76 @@ class PhProtocol(asyncio.Protocol):
                             logger.debug(i)
                 elif args[0] == 'query':
                     # If the user didn't specify any fields to return, return all attached to matched entries
-                    if not 'return' in cmd:
-                        cmd += ' return all'
+                    success, resp = parse_query(cmd)
+                    logger.error(f"parse responded with: {(success, resp)}")
+                    if not success:
+                        self.transport.write(to_bytes(nl(resp)))
+                        continue
 
-                    criteria = {}
-                    matches = ''
+                    criteria, return_fields = resp
+                    results = []
+                    _all = False
+                    entry = 0
+                    index = 0
+                    
+                    if "all" in return_fields:
+                        _all = True
+                        return_fields.remove("all")
 
-                    # If this isn't a syntactically-valid query, error out
-                    if re.search(r'(\S*)="([^"]*)"', cmd) is None:
-                        self.transport.write(to_bytes(nl('512:Illegal value. All queries need to be formed as [field]="[search]", ie universe="pennyverse".')))
-                        logger.warning('Client tried to send illegal query')
-                    else:
-                        # If this is a syntactically-valid query, go ahead and split it for processing
-                        matches = re.finditer(r'(\S*)="([^"]*)"', cmd)
+                    # These are to provide better error handling later on
+                    # Before, we were reliant on else statements for error handling and those didn't work across an entire dataset, just one entry
+                    found_one_match = False
+                    found_field = True
 
-                        for match in matches:
-                            criteria[match.group(1)] = match.group(2)
-
-                        # Check to make sure this field actually exists please
-                        find_all_fields()
-
-                        if not (list(criteria.keys())[0]) in unique_fields:
-                            self.transport.write(to_bytes(nl('507:Field does not exist.')))
-                            logger.warning('Desired field does not exist')
-                        else:
-                            _all = False
-                            return_fields = []
-
-                            # The if protects against an edge case where the user types "return" but specifies no fields
-                            if re.match(r'.* return', cmd) and not re.match(r'.* return (.*)', cmd):
-                                return_fields.append('all')
-                            # Splits the list of fields to return into its own list for later checking
-                            else:
-                                return_fields = re.match(r'.* return (.*)', cmd).group(1).split(' ')
-                                return_fields = always_fields + return_fields
-
-                            if 'all' in return_fields:
-                                _all = True
-                                return_fields.remove('all')
-
-                            results = []
-                            entry = 0
-                            index = 0
-
-                            # These are to provide better error handling later on
-                            # Before, we were reliant on else statements for error handling and those didn't work across an entire dataset, just one entry
-                            found_one_match = False
-                            found_field = False
-
-                            # Now we run through each item in entries.json to find matches
-                            for item in database:
-                                entry += 1
-                                # Defaults to "do not return" to avoid being able to dump the entire database
-                                # Which was possible with the first version :marf:
+                    # Now we run through each item in entries.json to find matches
+                    for item in database:
+                        # Defaults to "do not return" to avoid being able to dump the entire database
+                        # Which was possible with the first version :marf:
+                        
+                        # assuming query matches, then invalidating
+                        meets_criteria = True
+                        for (key, value) in criteria.items():
+                            if key not in item:
                                 meets_criteria = False
-                                for key in criteria:
-                                    if key in item:
-                                        if not criteria[key].lower() in item[key].lower():
-                                            meets_criteria = False
-                                            break
-                                        else:
-                                            meets_criteria = True
-                                            found_one_match = True
+                                break
+                            elif item[key].lower() != value.lower():
+                                meets_criteria = False
+                                break
+                            # if we get past, This remains valid :)
+                        if meets_criteria:
+                            found_one_match = True
+                            # We return a separate index number that counts up by one and NOT the number of the entry in the database
+                            # Netscape/Mosaic don't recognize the start of entries correctly if your index numbers aren't sequential, and phwin truncates fields for the same reason
+                            # Be careful if you're writing your own CCSO server please, this sucked to fix
+                            index = index + 1
+                            if _all:
+                                for (field, value) in item.items():
+                                    results.append('-200:' + str(index) + ': ' + field + ': ' + value)
+                            else:
+                                for field in return_fields:
+                                    if field not in filterable_fields or field not in item:
+                                        found_field = False
                                     else:
-                                        meets_criteria = False
-                                        break
-                                if meets_criteria:
-                                    # We return a separate index number that counts up by one and NOT the number of the entry in the database
-                                    # Netscape/Mosaic don't recognize the start of entries correctly if your index numbers aren't sequential, and phwin truncates fields for the same reason
-                                    # Be careful if you're writing your own CCSO server please, this sucked to fix
-                                    index = index + 1
-                                    if _all:
-                                        for field in list(database[entry - 1]):
-                                            found_field = True
-                                            results.append('-200:' + str(index) + ': ' + field + ': ' + database[entry - 1][field])
-                                    else:
-                                        for field in return_fields:
-                                            if (field in list(database[entry - 1])) and (field in filterable_fields) or _all:
-                                                found_field = True
-                                                results.append('-200:' + str(index) + ': ' + field + ': ' + database[entry - 1][field])
+                                        results.append('-200:' + str(index) + ': ' + field + ': ' + item[field])
 
-                            if not found_one_match:
-                                # If you found nothing, error out
-                                results.append(nl('502:No matches to query.'))
-                                logger.warning('Found no matches to client\'s query')
-                            elif not found_field:
-                                # We found something, just not what the user wanted
-                                results.append(nl('508:Field is not present in requested entries.'))
-                                logger.warning('Desired field is not present in the entries found')
+                    if not found_one_match:
+                        # If you found nothing, error out
+                        results.append(nl('502:No matches to query.'))
+                        logger.warning('Found no matches to client\'s query')
+                    elif not found_field:
+                        # We found something, just not what the user wanted
+                        results.append(nl('508:Field is not present in requested entries.'))
+                        logger.warning('Desired field is not present in the entries found')
 
-                            # Acknowledgement that the command finished regardless of result
-                            results.append(nl('200:Ok.'))
+                    # Acknowledgement that the command finished regardless of result
+                    results.append(nl('200:Ok.'))
 
-                            if logger.isEnabledFor(logging.DEBUG):
-                                for r in results:
-                                    logger.debug(r)
+                    if logger.isEnabledFor(logging.DEBUG):
+                        for r in results:
+                            logger.debug(r)
 
-                            resp = to_bytes(results)
-                            self.transport.write(resp)
+                    resp = to_bytes(results)
+                    self.transport.write(resp)
                 # If the user inputs a quit command, terminate the connection
                 elif args[0] in ['quit', 'stop', 'exit']:
                     self.transport.write(to_bytes(nl('200:Bye!')))
